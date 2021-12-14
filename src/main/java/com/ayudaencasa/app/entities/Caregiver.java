255d720de25ec98afec011bdc438e046f83aaec1
Dieego12 <dcostantino23@gmail.com>
2021-12-14 20:05:58 -0300

    Update Caregiver.java

--- src/main/java/com/ayudaencasa/app/entities/Caregiver.java
+++ src/main/java/com/ayudaencasa/app/entities/Caregiver.java
@@ -1,6 +1,37 @@
 
 package com.ayudaencasa.app.entities;
 
-public class Caregiver {
+import javafx.util.Pair;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE categories SET deleted_at = current_timestamp() WHERE id = ?")
+@Where(clause = "deleted_at is null")
+
+public class Caregiver extends Job {
+
+    private Integer quantity;
+    
+    private Pair<Integer, Integer> ageRange;
+    
+    private Boolean cooking;
+    
+    private Boolean cleaningPeople;
+    
+    private Boolean tranfering;
+    
+    
+    @Override
+    public String getType() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
