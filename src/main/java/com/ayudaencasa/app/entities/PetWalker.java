a261ed524393770051895fea9222b2fbf5fe2410
martinamieras <91919491+martinamieras@users.noreply.github.com>
2021-12-14 19:52:24 -0300

    Update PetWalker.java

--- src/main/java/com/ayudaencasa/app/entities/PetWalker.java
+++ src/main/java/com/ayudaencasa/app/entities/PetWalker.java
@@ -1,6 +1,7 @@
 
 package com.ayudaencasa.app.entities;
 
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,7 @@ import org.hibernate.annotations.Where;
 @Setter
 @SQLDelete(sql = "UPDATE categories SET deleted_at = current_timestamp() WHERE id = ?")
 @Where(clause = "deleted_at is null")
-public class PetWalker {
+public class PetWalker extends Job{
     
     private Integer petQuantity;
     
@@ -22,5 +23,36 @@ public class PetWalker {
     
     private String petRace;
     
-    public abstract PetWalker getType();
+
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.getType());
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PetWalker other = (PetWalker) obj;
+        if (!Objects.equals(this.getType(), other.getType())) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String getType() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
