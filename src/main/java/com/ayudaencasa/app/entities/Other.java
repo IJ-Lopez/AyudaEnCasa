4866cc64d5d6f52f07f9c85641175436bd7049e1
martinamieras <91919491+martinamieras@users.noreply.github.com>
2021-12-14 20:52:11 -0300

    Create Other.java

---
+++ src/main/java/com/ayudaencasa/app/entities/Other.java
@@ -0,0 +1,17 @@
+
+package com.ayudaencasa.app.entities;
+
+public class Other extends Job {
+
+    public String type;
+    
+    
+    
+    @Override
+    public String getType() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
