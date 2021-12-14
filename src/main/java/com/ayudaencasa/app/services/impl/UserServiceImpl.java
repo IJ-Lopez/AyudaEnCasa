06ec314af4b5492c2d71eebf8b77fa4fc2d5ca91
Ignacio Lòpez <agsdf12341@gmail.com>
2021-12-14 20:27:41 -0300

    User Repository created

--- src/main/java/com/ayudaencasa/app/services/impl/UserServiceImpl.java
+++ src/main/java/com/ayudaencasa/app/services/impl/UserServiceImpl.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ayudaencasa.app.services.impl;
 
-/**
- *
- * @author lopez
- */
+import com.ayudaencasa.app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl {
     
+    @Autowired
+    private UserRepository userRepo;
 }
