4c512c94d53da3d6cb1a8c5135b91c0776c3f08a
martinamieras <91919491+martinamieras@users.noreply.github.com>
2021-12-27 18:44:07 -0300

    Update PetWalkerController.java

--- src/main/java/com/ayudaencasa/app/controllers/PetWalkerController.java
+++ src/main/java/com/ayudaencasa/app/controllers/PetWalkerController.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.ayudaencasa.app.controllers;
+
+import com.ayudaencasa.app.entities.PetWalker;
+import com.ayudaencasa.app.services.PetWalkerService;
+import javax.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author martina
  */
+@RestController
+@Validated
+@RequestMapping ("/petWalker")
 public class PetWalkerController {
+    
+    @Autowired
+    private PetWalkerService petWalkerService;
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public PetWalker create (@Valid @RequestBody createPetWalkerDTO inputPetWalker){
+        PetWalker petWalker = new PetWalker();
+        BeanUtils.copyProperties(inputPetWalker,petWalker );
+        return petWalkerService.create(petWalker);
+    }
+    
+    public List <PetWalker> findAll (@RequestParam)
+    
+    
     
 }
