2d10eb49daa3ce86bea11cdcb3264589296e7dcc
martinamieras <91919491+martinamieras@users.noreply.github.com>
2021-12-27 19:05:33 -0300

    Update PetWalkerController.java

--- src/main/java/com/ayudaencasa/app/controllers/PetWalkerController.java
+++ src/main/java/com/ayudaencasa/app/controllers/PetWalkerController.java
@@ -3,14 +3,17 @@ package com.ayudaencasa.app.controllers;
 
 import com.ayudaencasa.app.entities.PetWalker;
 import com.ayudaencasa.app.services.PetWalkerService;
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,9 +36,23 @@ public class PetWalkerController {
         BeanUtils.copyProperties(inputPetWalker,petWalker );
         return petWalkerService.create(petWalker);
     }
+    @GetMapping
+    public List <PetWalker> findAll (@RequestParam (required = false) String q){
+        return petWalkerService.findAll();
+    }
+    @GetMapping
+    public PetWalker findById (String id){
+        return petWalkerService.findById(id);
+    }
     
-    public List <PetWalker> findAll (@RequestParam)
+    @GetMapping
+    public void delete (String id){
+        petWalkerService.delete(id);
+    }
     
-    
-    
+    @PostMapping
+    public void update (String id, PetWalker newPetWalker){
+        petWalkerService.update(id, newPetWalker);
+        
+    }
 }
