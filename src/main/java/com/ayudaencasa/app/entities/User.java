322e0f462be7afa735ac3c02d2492ff9ccdd5f13
Sebastian <seba_al2007@hotmail.com>
2022-01-17 23:30:43 -0300

    Update User.java

--- src/main/java/com/ayudaencasa/app/entities/User.java
+++ src/main/java/com/ayudaencasa/app/entities/User.java
@@ -3,7 +3,6 @@ package com.ayudaencasa.app.entities;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
