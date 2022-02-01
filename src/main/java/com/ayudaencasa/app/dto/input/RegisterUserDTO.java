2ee0f3c0805f881dea73467e81cdc61ad710a2b2
Dieego12 <dcostantino23@gmail.com>
2022-01-31 22:13:15 -0300

    Update RegisterUserDTO.java

--- src/main/java/com/ayudaencasa/app/dto/input/RegisterUserDTO.java
+++ src/main/java/com/ayudaencasa/app/dto/input/RegisterUserDTO.java
@@ -1,17 +1,23 @@
 package com.ayudaencasa.app.dto.input;
 
 import java.util.Date;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
+@Getter
 public class RegisterUserDTO {
 
+    @NotNull
     private String email;
+    private String password2;
     private String password;
     private String firstName;
     private String lastName;
+    private String departament;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
     private Integer dni;
